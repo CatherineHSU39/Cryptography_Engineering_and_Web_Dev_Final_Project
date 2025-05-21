150202f1e1e37762e3f03e1f0df9fb29444acda6
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-21 17:21:38 +0800

    fix(auth): incorrect return type of loading user detail
    - Should be CustomUserDetails instead of the default UserDetails

--- auth/src/main/java/com/nycu/ce/ciphergame/auth/service/CustomUserDetailsService.java
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/service/CustomUserDetailsService.java
@@ -3,24 +3,25 @@ package com.nycu.ce.ciphergame.auth.service;
 import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.nycu.ce.ciphergame.auth.entity.User;
 import com.nycu.ce.ciphergame.auth.repository.UserRepository;
+import com.nycu.ce.ciphergame.auth.security.CustomUserDetails;
 @Service
 public class CustomUserDetailsService  implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException("User Not Found with username: " + username);
         }
-        return new org.springframework.security.core.userdetails.User(
+        return new CustomUserDetails(
+                user.getId(),
                 user.getUsername(),
                 user.getPassword(),
                 Collections.emptyList()
