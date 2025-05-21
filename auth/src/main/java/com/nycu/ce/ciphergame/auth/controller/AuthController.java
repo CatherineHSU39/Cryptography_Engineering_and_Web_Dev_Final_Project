b38844997283600a2c19c31b791ca2cf826dd7b8
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-21 16:12:52 +0800

    edit(auth): define auth api
    - /auth/signin
    - /auth/register

---
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.nycu.ce.ciphergame.auth.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nycu.ce.ciphergame.auth.dto.UserRegisterRequest;
+import com.nycu.ce.ciphergame.auth.dto.UserSigninRequest;
+import com.nycu.ce.ciphergame.auth.dto.UserSigninResponse;
+import com.nycu.ce.ciphergame.auth.security.CustomUserDetails;
+import com.nycu.ce.ciphergame.auth.security.JwtUtil;
+import com.nycu.ce.ciphergame.auth.service.UserService;
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    UserService userService;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    JwtUtil jwtUtils;
+    @PostMapping("/signin")
+    public ResponseEntity<UserSigninResponse> authenticateUser(@RequestBody UserSigninRequest user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),
+                        user.getPassword()
+                )
+        );
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        String token = jwtUtils.generateToken(userDetails);
+        UserSigninResponse response = new UserSigninResponse();
+        response.setUsername(userDetails.getUsername());
+        response.setRole(userDetails.getAuthorities().toString());
+        response.setToken(token);
+        return ResponseEntity.ok(response);
+    }
+    @PostMapping("/register")
+    public ResponseEntity<Void> registerUser(@RequestBody UserRegisterRequest request) {
+        userService.createUser(request);
+        return ResponseEntity.ok().build();
+    }
+}
+
