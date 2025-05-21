6615a733b0a0af9300ff56b39df7a1741b407f53
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-21 16:08:18 +0800

    edit(auth): define user service

---
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/service/UserService.java
@@ -0,0 +1,37 @@
+package com.nycu.ce.ciphergame.auth.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.nycu.ce.ciphergame.auth.dto.UserRegisterRequest;
+import com.nycu.ce.ciphergame.auth.dto.UserSigninResponse;
+import com.nycu.ce.ciphergame.auth.entity.User;
+import com.nycu.ce.ciphergame.auth.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    public UserSigninResponse createUser(UserRegisterRequest request) {
+        String username = request.getUsername();
+        String password = request.getPassword();
+        if (userRepository.existsByUsername(username)) {
+            throw new RuntimeException("User already exists");
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(encoder.encode(password));
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+
+        UserSigninResponse response = new UserSigninResponse();
+        response.setUsername(user.getUsername());
+        response.setRole(user.getRole());
+        return response;
+    }
+}
