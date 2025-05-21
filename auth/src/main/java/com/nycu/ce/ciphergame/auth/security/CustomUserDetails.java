b4bcd74e9268201e659702d9178e673e9dff91a0
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-21 18:16:52 +0800

    edit(auth): update customUserDetails
    - construct with User instead of multiple columns
    - add `ROLE` prefix when getAuthorities

--- auth/src/main/java/com/nycu/ce/ciphergame/auth/security/CustomUserDetails.java
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/security/CustomUserDetails.java
@@ -1,23 +1,27 @@
 package com.nycu.ce.ciphergame.auth.security;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.nycu.ce.ciphergame.auth.entity.User;
+
 public class CustomUserDetails implements UserDetails {
+
     private final UUID id;
     private final String username;
     private final String password;
-    private final Collection<? extends GrantedAuthority> authorities;
+    private final String role;
 
-    public CustomUserDetails(UUID id, String username, String password, 
-                             Collection<? extends GrantedAuthority> authorities) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.authorities = authorities;
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.role = user.getRole();
     }
 
     public UUID getId() {
@@ -25,20 +29,12 @@ public class CustomUserDetails implements UserDetails {
     }
 
     @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
     }
 
+    @Override public String getPassword() { return password; }
+    @Override public String getUsername() { return username; }
     @Override public boolean isAccountNonExpired() { return true; }
     @Override public boolean isAccountNonLocked() { return true; }
     @Override public boolean isCredentialsNonExpired() { return true; }
