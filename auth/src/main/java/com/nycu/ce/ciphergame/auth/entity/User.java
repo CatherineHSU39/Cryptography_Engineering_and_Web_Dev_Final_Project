68dcab0a4316e1279ccdc2a72f27bca3fa85c22f
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-21 16:06:48 +0800

    edit(auth): define user entity

---
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/entity/User.java
@@ -0,0 +1,47 @@
+package com.nycu.ce.ciphergame.auth.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false)
+    private String password;
+
+    @Column(name = "encrypted_totp_secret", nullable = false)
+    private String totpSecret;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(
+        name = "created_at", 
+        nullable = false, 
+        updatable = false
+        )
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
