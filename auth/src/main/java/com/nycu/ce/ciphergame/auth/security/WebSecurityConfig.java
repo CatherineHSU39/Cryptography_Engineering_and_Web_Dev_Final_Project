e1918a33eebbbbf9af651d034a60b96afd6a65e0
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-22 12:24:33 +0800

    edit(auth): require jwt authentication for 2FA varification

--- auth/src/main/java/com/nycu/ce/ciphergame/auth/security/WebSecurityConfig.java
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.nycu.ce.ciphergame.auth.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,7 +29,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable()) // Optional: Enable CORS if frontend interacts directly
+                .cors(cors -> cors.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
@@ -36,9 +37,13 @@ public class WebSecurityConfig {
                         "/auth/login",
                         "/.well-known/jwks.json",
                         "/swagger-ui/**",
-                        "/v3/api-docs/**")
-                .permitAll()
-                .anyRequest().authenticated());
+                        "/v3/api-docs/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(Customizer.withDefaults()) // enable JWT parsing
+                );
 
         return http.build();
     }
