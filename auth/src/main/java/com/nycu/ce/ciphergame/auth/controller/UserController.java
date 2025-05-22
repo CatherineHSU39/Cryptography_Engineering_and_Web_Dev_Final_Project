9881ce57c18440cdc85c198683ba4ea364713d27
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-22 15:47:20 +0800

    chore(auth): refactor authentication and 2FA APIs to follow RESTful conventions

--- auth/src/main/java/com/nycu/ce/ciphergame/auth/controller/UserController.java
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/controller/UserController.java
@@ -7,15 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nycu.ce.ciphergame.auth.dto.UserRegisterRequest;
 import com.nycu.ce.ciphergame.auth.service.GAService;
 import com.nycu.ce.ciphergame.auth.service.UserService;
 
 @RestController
-@RequestMapping("/2fa")
-public class TwoFaController {
+@RequestMapping("/users")
+public class UserController {
 
     @Autowired
     GAService gaService;
@@ -23,7 +26,13 @@ public class TwoFaController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/setup")
+    @PostMapping
+    public ResponseEntity<Void> registerUser(@RequestBody UserRegisterRequest request) {
+        userService.createUser(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/me/2fa")
     public ResponseEntity<String> setup2FA(@AuthenticationPrincipal Jwt jwt) {
         String secret = gaService.generateKey();
         UUID userId = UUID.fromString(jwt.getSubject());
