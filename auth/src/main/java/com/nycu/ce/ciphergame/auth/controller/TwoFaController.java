d28eb0e53df65a36348f3fb7000be0921f3b0f00
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-22 12:12:18 +0800

    edit(auth): correct the api path and saveTotpSecret by user id

--- auth/src/main/java/com/nycu/ce/ciphergame/auth/controller/TwoFaController.java
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/controller/TwoFaController.java
@@ -3,12 +3,16 @@ package com.nycu.ce.ciphergame.auth.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.nycu.ce.ciphergame.auth.security.CustomUserDetails;
 import com.nycu.ce.ciphergame.auth.service.GAService;
 import com.nycu.ce.ciphergame.auth.service.UserService;
 
+@RestController
+@RequestMapping("/2fa")
 public class TwoFaController {
 
     @Autowired
@@ -17,10 +21,10 @@ public class TwoFaController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/2fa/setup")
-    public ResponseEntity<String> setup2FA(@AuthenticationPrincipal UserDetails userDetails) {
+    @PostMapping("/setup")
+    public ResponseEntity<String> setup2FA(@AuthenticationPrincipal CustomUserDetails userDetails) {
         String secret = gaService.generateKey();
-        userService.saveTotpSecret(userDetails.getUsername(), secret);
+        userService.saveTotpSecret(userDetails.getId(), secret);
         String qrCode = gaService.generateQRUrl(userDetails.getUsername(), secret);
         return ResponseEntity.ok(qrCode);
     }
