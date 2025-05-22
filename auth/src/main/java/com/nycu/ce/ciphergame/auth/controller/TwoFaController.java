565491175b5b6c55dc3bf73eb0ec71f9216c37e2
CatherineHSU39 <a0713339.mg07@nycu.edu.tw>
2025-05-22 14:01:51 +0800

    edit(auth): authenticate with jwt instead of customUserDetails

--- auth/src/main/java/com/nycu/ce/ciphergame/auth/controller/TwoFaController.java
+++ auth/src/main/java/com/nycu/ce/ciphergame/auth/controller/TwoFaController.java
@@ -1,13 +1,15 @@
 package com.nycu.ce.ciphergame.auth.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nycu.ce.ciphergame.auth.security.CustomUserDetails;
 import com.nycu.ce.ciphergame.auth.service.GAService;
 import com.nycu.ce.ciphergame.auth.service.UserService;
 
@@ -22,10 +24,11 @@ public class TwoFaController {
     UserService userService;
 
     @PostMapping("/setup")
-    public ResponseEntity<String> setup2FA(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<String> setup2FA(@AuthenticationPrincipal Jwt jwt) {
         String secret = gaService.generateKey();
-        userService.saveTotpSecret(userDetails.getId(), secret);
-        String qrCode = gaService.generateQRUrl(userDetails.getUsername(), secret);
+        UUID userId = UUID.fromString(jwt.getSubject());
+        userService.saveTotpSecret(userId, secret);
+        String qrCode = gaService.generateQRUrl(secret, userId);
         return ResponseEntity.ok(qrCode);
     }
 }
